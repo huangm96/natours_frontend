f042613160c5cbc092110bb7d8e7ea0a7b55d2ad
Min-Huang <minh.huang96@gmail.com>
2022-05-23 21:03:28 -0700

    finished card data

--- src/components/tourCard/CardData.js
+++ src/components/tourCard/CardData.js
@@ -1,9 +1,13 @@
 import React from "react";
+import "./CardData.css";
 
-function CardData({ icon, data }) {
+function CardData({ Icon, data }) {
   return (
-    <div className="cardData-container">
-      <icon />
+    <div className="card-data-container">
+      {Icon ? (
+        <Icon size="1.5rem" color="#7dd56f" className="card-data-icon" />
+      ) : null}
+
       <span>{data}</span>
     </div>
   );
--- src/components/tourCard/CardDetails.js
+++ src/components/tourCard/CardDetails.js
@@ -1,24 +1,45 @@
 import React from "react";
 import "./CardDetails.css";
 import CardData from "./CardData";
-import { GrLocation } from "react-icons/gr";
-
+import {
+  BsFillGeoAltFill,
+  BsPerson,
+  BsFlag,
+  BsCalendarEvent,
+} from "react-icons/bs";
 function CardDetails({ tour }) {
   const tourStartDate = new Date(tour.startDates[0]);
-  console.log(tourStartDate);
+  const month = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
   return (
     <div className="card-details-container">
-      <p className="card-subheading">{`${tour.difficulty} ${tour.duration}-day tour`}</p>
-      <div>{tour.summary}</div>
-      <div>
-        <CardData icon={GrLocation} data={tour.startLocation.description} />
+      <p className="card-details-subheading">{`${tour.difficulty} ${tour.duration}-day tour`}</p>
+      <div className="card-details-summary">{tour.summary}</div>
+      <div className="card-details-data">
         <CardData
-          data={`${tourStartDate.getMonth() + 1} ${
+          data={tour.startLocation.description}
+          Icon={BsFillGeoAltFill}
+        />
+        <CardData
+          data={`${month[tourStartDate.getMonth()]} ${
             tourStartDate.getYear() + 1900
           }`}
+          Icon={BsCalendarEvent}
         />
-        <CardData data={`${tour.locations.length} stops`} />
-        <CardData data={`${tour.maxGroupSize} people`} />
+        <CardData data={`${tour.locations.length} stops`} Icon={BsFlag} />
+        <CardData data={`${tour.maxGroupSize} people`} Icon={BsPerson} />
       </div>
     </div>
   );
