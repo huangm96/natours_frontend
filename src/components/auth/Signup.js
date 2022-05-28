1d4c0295b0a0979a9c2b25c9095d205cc7398413
Min-Huang <minh.huang96@gmail.com>
2022-05-27 22:06:58 -0700

    working on forms

--- src/components/auth/Signup.js
+++ src/components/auth/Signup.js
@@ -1,7 +1,53 @@
 import React from "react";
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import { MyTextInput } from "../form/FormElements";
+const Signup = () => {
+  return (
+    <Formik
+      initialValues={{ firstName: "", lastName: "", email: "" }}
+      validationSchema={Yup.object({
+        firstName: Yup.string()
+          .max(15, "Must be 15 characters or less")
+          .required("Required"),
+        lastName: Yup.string()
+          .max(20, "Must be 20 characters or less")
+          .required("Required"),
+        email: Yup.string().email("Invalid email address").required("Required"),
+      })}
+      onSubmit={(values, { setSubmitting }) => {
+        setTimeout(() => {
+          alert(JSON.stringify(values, null, 2));
+          setSubmitting(false);
+        }, 400);
+      }}
+    >
+      <Form>
+        <MyTextInput
+          label="First Name"
+          name="firstName"
+          type="text"
+          placeholder="Jane"
+        />
 
-function Signup() {
-  return <div className="signup">Signup</div>;
-}
+        <MyTextInput
+          label="Last Name"
+          name="lastName"
+          type="text"
+          placeholder="Doe"
+        />
+
+        <MyTextInput
+          label="Email Address"
+          name="email"
+          type="email"
+          placeholder="jane@formik.com"
+        />
+
+        <button type="submit">Submit</button>
+      </Form>
+    </Formik>
+  );
+};
 
 export default Signup;
