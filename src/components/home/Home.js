e7be2c35573e6f49b823ca18401b6d7dbe3380ca
Min-Huang <minh.huang96@gmail.com>
2022-06-28 15:05:52 -0700

    fix bugs

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 
-import Header from "./../header/Header";
-import Footer from "./../footer/Footer";
+import Header from "./../header/Header.js";
+import Footer from "./../footer/Footer.js";
 function Home() {
   return (
     <div
