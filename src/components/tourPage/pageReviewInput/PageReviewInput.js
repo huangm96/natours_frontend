16a4dd9ab7484f17057e33fcaa776ca416f73b27
Min-Huang <minh.huang96@gmail.com>
2022-06-27 21:58:43 -0700

    if no user information, hide the input and ask them to register or login

--- src/components/tourPage/pageReviewInput/PageReviewInput.js
+++ src/components/tourPage/pageReviewInput/PageReviewInput.js
@@ -1,20 +1,29 @@
 import React, { useContext } from "react";
+import { Link } from "react-router-dom";
 import AuthContext from "../../../context/AuthContext";
 import { displayUserAvatar } from "../../userAvatar/userAvatar";
 import PageReviewInputForm from "./PageReviewInputForm";
 import "./PageReviewInput.css";
 const PageReviewInput = ({ tour }) => {
   const { user } = useContext(AuthContext);
-  if (user && user.id) {
-    return (
-      <div className="page-review-input-container">
-        {displayUserAvatar(user)}
 
-        <PageReviewInputForm tour={tour} />
-      </div>
-    );
-  }
-  return null;
+  return (
+    <div className="page-review-input-container">
+      {user && user.id ? (
+        <>
+          {displayUserAvatar(user)}
+
+          <PageReviewInputForm tour={tour} />
+        </>
+      ) : (
+        <div>
+          <span>You must </span> <Link to="/signup">register</Link>{" "}
+          <span> or </span>
+          <Link to="/login">login</Link> to post a comment.
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default PageReviewInput;
